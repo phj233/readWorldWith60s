7893bc970f5cd7f0ba78f76d4895004a77143f83
phj233 <2780990934@qq.com>
2022-05-07 18:27:12 +0800

    Update index.js

--- ReadWorldWith60s/index.js
+++ ReadWorldWith60s/index.js
@@ -1,24 +1,40 @@
-const request = require("sync-request");
-const { segment } = require('oicq');
-
-const api60s='http://api.03c3.cn/zb';
+const {segment} = require('oicq');
+const request = require('sync-request');
+const api60s='http://api.qemao.com/api/60s/';
+const api60s2='https://api.iyk0.com/60s/';
 const cmd='今日60s';
 
+function returnImgurl(apiurl){
+    var img = request('GET',apiurl);
+    var returnData = JSON.parse(img.getBody('utf8'));
+    return returnData.imageUrl;
+}
+function retrunImg(){
+    let imgurl = api60s;
+    let img60s=segment.image(imgurl);  
+    //let img60s=segment.image(returnImgurl(api60s2));  // 如果图片发不出来 就把这行代码前面'//'删掉 再把上一行代码前面加上'//'
+    return img60s;
+}
 class ReadWorldWith60s extends NIL.ModuleBase{
     onStart(api){
-        api.logger.info('60s读世界加载成功 by phj233~');
+        api.logger.info('60s读世界加载成功 by phj233~~');
         api.listen('onGroupMessageReceived',(e)=>{
             if(e.raw_message==cmd){
-                let imgurl = api60s;
-                let img60s=segment.image(imgurl);
-                img60s.asface = true;
-                e.reply(img60s,img60s);
+                e.reply([`今日60s读懂世界:`,retrunImg()],`~`);
             }
+        
         });
+        api.listen('onGroupMessageReceived',(o)=>{
+            var id =setInterval(function(){
+                if (new Date().getMinutes()==3 &&new Date().getHours()==11&&new Date().getSeconds()==0){
+                    o.reply([`今日60s读懂世界:`,retrunImg()]);
+                }
+            },1000,id)
+        })
     }
     onStop(){
    
     }
    }
    
-   module.exports = new ReadWorldWith60s;
+   module.exports = new ReadWorldWith60s
